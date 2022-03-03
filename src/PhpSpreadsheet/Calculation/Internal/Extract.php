87a2f4753d351156f11aaa6530d1e2125f1238e6
MarkBaker <mark@lange.demon.co.uk>
2022-03-03 13:51:18 +0100

    Initial work on refactoring branch pruning logic

---
+++ src/PhpSpreadsheet/Calculation/Internal/Extract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PhpOffice\PhpSpreadsheet\Calculation\Internal;
+
+class Extract
+{
+    /**
+     * @param mixed ...$args
+     */
+    public static function arguments(...$args): array
+    {
+        return $args;
+    }
+}
